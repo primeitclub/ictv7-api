6fa57d9d3811742850909c2250585e132d1e0b8e
iamsampang <iamsampang@gmail.com>
2023-11-11 21:49:40 +0545

    feat: install passport and passport-google-oauth for authentication

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,3 +6,8 @@ export const env = {
   port: process.env.PORT || 6969,
   version: process.env.VERSION || 'alpha'
 };
+
+export const googleClient = {
+  id: process.env.GOOGLE_CLIENT_ID,
+  secret: process.env.GOOGLE_CLIENT_SECRET
+};
