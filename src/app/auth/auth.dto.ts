14849e8a46d0c9855ac995a22ed2eade2416c30d
iamsampang <iamsampang@gmail.com>
2023-11-14 18:55:56 +0545

    feat: add basic scaffolding for authentication

---
+++ src/app/auth/auth.dto.ts
@@ -0,0 +1,91 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsPhoneNumber } from 'class-validator';
+
+export class registerUserDTO {
+  @ApiProperty()
+  @IsNotEmpty({
+    message: 'Username field is required.'
+  })
+  username: string;
+
+  @ApiProperty()
+  @IsNotEmpty({
+    message: 'Email field is required.'
+  })
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty({
+    message: 'Password field is required.'
+  })
+  password: string;
+
+  @ApiProperty()
+  @IsNotEmpty({
+    message: 'Phone number field is required.'
+  })
+  @IsPhoneNumber('NE', {
+    message: 'Phone number must be a valid phone number.'
+  })
+  phone: string;
+
+  @ApiProperty()
+  @IsNotEmpty({
+    message: 'Address field is required.'
+  })
+  address: string;
+
+  @ApiProperty()
+  @IsNotEmpty({
+    message: 'Terms and conditions field is required.'
+  })
+  TnCFlag: boolean;
+}
+
+export class loginUserDTO {
+  @ApiProperty()
+  @IsNotEmpty({
+    message: 'Email field is required.'
+  })
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty({
+    message: 'Password field is required.'
+  })
+  password: string;
+}
+
+export class forgotPasswordDTO {
+  @ApiProperty()
+  @IsNotEmpty({
+    message: 'Email field is required.'
+  })
+  email: string;
+}
+
+export class resetPasswordDTO {
+  @ApiProperty()
+  @IsNotEmpty({
+    message: 'Id field is required.'
+  })
+  id: string;
+
+  @ApiProperty()
+  @IsNotEmpty({
+    message: 'Token field is required.'
+  })
+  token: string;
+
+  @ApiProperty()
+  @IsNotEmpty({
+    message: 'Password field is required.'
+  })
+  password: string;
+
+  @ApiProperty()
+  @IsNotEmpty({
+    message: 'Password confirmation field is required.'
+  })
+  passwordConfirmation: string;
+}
