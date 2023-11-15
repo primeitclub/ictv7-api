ba0d6db7789233d710b6cf47e561fa6e7f589f69
iamsampang <iamsampang@gmail.com>
2023-11-15 19:00:57 +0545

    feat: implement user registration feature

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './model/User.entity';
+import { Repository } from 'typeorm';
+import { UserType } from './user.enum';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) {}
+
+  async createUser(user: {
+    username: string;
+    email: string;
+    phone: string;
+    password: string;
+    address: string;
+    user_type: UserType;
+    college_name: string;
+    TnCFlag: boolean;
+  }) {
+    const newUser = await this.userRepository.save(user);
+    return newUser;
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  async getUser(id: string) {
+    return this.userRepository.findOne({ where: { id } });
+  }
+}
