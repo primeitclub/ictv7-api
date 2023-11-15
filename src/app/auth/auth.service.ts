6a7033a5ae1112d250ac3d84c0b89cd362f4e858
iamsampang <iamsampang@gmail.com>
2023-11-15 19:06:24 +0545

    refactor: extract the registration logic to service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
+import { registerUserDTO } from './auth.dto';
+import { UserService } from '../user/user.service';
+import { generateHashedPassword } from '../user/bcrypt.helper';
 
 @Injectable()
 export class AuthService {
-  async signInWithGoogle(req) {
-    console.log(req.user);
+  constructor(private readonly userService: UserService) {}
+
+  async register(@Body() request: registerUserDTO) {
+    const {
+      username,
+      email,
+      phone,
+      password,
+      address,
+      user_type,
+      college_name,
+      TnCFlag
+    } = request;
+    const hashedPassword = await generateHashedPassword(password);
+
+    const user = await this.userService.createUser({
+      username,
+      email,
+      phone,
+      password: hashedPassword,
+      user_type,
+      college_name,
+      address,
+      TnCFlag
+    });
+
+    return {
+      message: 'You have been registered successfully.',
+      data: user
+    };
   }
 }
