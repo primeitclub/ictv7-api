3ae0ffecf4068d61fa33d586e8733f2e23f04bdd
iamsampang <iamsampang@gmail.com>
2023-11-17 22:20:10 +0545

    chore: rename bcrypt utility functions for clarity

--- src/utils/bcrypt.util.ts
+++ src/utils/bcrypt.util.ts
@@ -2,16 +2,14 @@ import * as bcrypt from 'bcryptjs';
 
 const { compare, genSalt, hash } = bcrypt;
 
-export const generateHashedPassword = async (
-  passwordString: string
-): Promise<string> => {
+export const hashInformation = async (input: string): Promise<string> => {
   const salt = await genSalt(10);
-  return hash(passwordString, salt);
+  return hash(input, salt);
 };
 
-export const comparePassword = async (
-  password: string,
+export const compareHashedInformation = async (
+  input: string,
   hashString: string
 ): Promise<boolean> => {
-  return compare(password, hashString);
+  return compare(input, hashString);
 };
