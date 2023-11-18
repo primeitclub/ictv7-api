4e09f056b2821a3ecd2cc5901dfe2b4cd4167e0e
iamsampang <iamsampang@gmail.com>
2023-11-18 20:20:33 +0545

    feat: assign OTP code to a registered user

--- src/app/user/model/Otp.entity.ts
+++ src/app/user/model/Otp.entity.ts
@@ -3,22 +3,23 @@ import {
   PrimaryGeneratedColumn,
   Column,
   ManyToOne,
+  CreateDateColumn,
   JoinColumn
 } from 'typeorm';
 import { User } from './User.entity'; // Import the User entity
 
 @Entity()
 export class OTP {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column({ type: 'varchar', name: 'OTP number' })
-  otpNumber: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Column({ type: 'varchar' })
-  expiration: string;
+  otpNumber: number;
 
-  @Column({ type: 'varchar', name: 'created_at' })
+  @Column()
+  expiresAt: Date;
+
+  @CreateDateColumn()
   createdAt: string;
 
   @ManyToOne(() => User, (user) => user.otps) // Assuming you have a field named 'otps' in your User entity
