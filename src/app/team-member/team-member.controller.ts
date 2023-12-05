9740f886ee9566fb0802ae672324783dce6f804f
iambasantarai <iambasantarai@gmail.com>
2023-12-05 21:10:17 +0545

    feat: implement crud functionality for team members

--- src/app/team-member/team-member.controller.ts
+++ src/app/team-member/team-member.controller.ts
@@ -1,4 +1,101 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt.guard';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { TeamMemberService } from './team-member.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
-@Controller('team-member')
-export class TeamMemberController {}
+@ApiBearerAuth()
+@ApiTags('Team Members')
+@Controller('team-members')
+export class TeamMemberController {
+  constructor(private teamMemberService: TeamMemberService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async handleGetMembers() {
+    return this.teamMemberService.getMembers();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      title: 'addTeamMember',
+      required: ['fullName', 'memberType', 'image'],
+      properties: {
+        fullName: {
+          type: 'string'
+        },
+        memberType: {
+          type: 'enum'
+        },
+        image: {
+          type: 'string',
+          format: 'binary'
+        }
+      }
+    }
+  })
+  @UseInterceptors(FileInterceptor('image'))
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async handleAddMember(
+    @Body() request,
+    @UploadedFile() image: Express.Multer.File
+  ) {
+    return this.teamMemberService.addMember(request, image.path);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      title: 'updateTeamMember',
+      properties: {
+        fullName: {
+          type: 'string'
+        },
+        memberType: {
+          type: 'enum'
+        },
+        image: {
+          type: 'string',
+          format: 'binary'
+        }
+      }
+    }
+  })
+  @UseInterceptors(FileInterceptor('image'))
+  @Put('/:id')
+  @HttpCode(HttpStatus.OK)
+  async handleUpdateMember(
+    @Param('id') id: string,
+    @UploadedFile() image: Express.Multer.File,
+    @Body() request
+  ) {
+    return this.teamMemberService.updateMember(id, request, image.path);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('/:id')
+  @HttpCode(HttpStatus.OK)
+  async handleDeleteMember(@Param('id') id: string) {
+    return this.teamMemberService.deleteMember(id);
+  }
+}
--- src/app/team-member/team-member.module.ts
+++ src/app/team-member/team-member.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TeamMemberService } from './team-member.service';
 import { TeamMemberController } from './team-member.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TeamMember } from './TeamMember.entity';
+import { MulterModule } from '@nestjs/platform-express';
+import { multerConfig } from 'src/config/multer.config';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([TeamMember]),
+    MulterModule.register(multerConfig)
+  ],
   providers: [TeamMemberService],
   controllers: [TeamMemberController]
 })
