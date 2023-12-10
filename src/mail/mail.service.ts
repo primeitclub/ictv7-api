4fac2907b1b7faab339bb818797684e2dabcd99d
x-nimesh <x.nimesh7@gmail.com>
2023-12-10 13:58:29 +0545

    otp

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,6 +1,8 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { smtp } from 'src/config/env';
+import { mailOtp } from './templates/otp.template';
+import { OTP } from 'src/app/user/model/Otp.entity';
 
 @Injectable()
 export class MailService {
@@ -12,6 +14,21 @@ export class MailService {
       from: `PRIME IT CLUB <${smtp.user}>`,
       subject: subject,
       text: mailContent
+    });
+
+    console.log('MAIL SENT TO: ', receiver);
+  }
+  async sendOTPEmail(
+    receiver: string,
+    subject: string,
+    // mailTemplate: string,
+    data?: any
+  ) {
+    this.mailerService.sendMail({
+      to: receiver,
+      from: `PRIME IT CLUB <${smtp.user}>`,
+      subject: subject,
+      html: mailOtp(data)
     });
 
     console.log('MAIL SENT TO: ', receiver);
