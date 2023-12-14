f66ebcb54eb14e71720dc5fe3e761de36e2e7c88
x-nimesh <x.nimesh7@gmail.com>
2023-12-14 10:24:10 +0545

    Refactor esports controller and service
    Add multer configuration for esports module
    
    Update OTP email template

--- src/app/esports/esports.module.ts
+++ src/app/esports/esports.module.ts
@@ -4,9 +4,14 @@ import { EsportsService } from './esports.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EsportesGame } from './models/Esports.entity';
 import { EsportsTeam } from './models/EsportsTeam.entity';
+import { MulterModule } from '@nestjs/platform-express';
+import { esportsMulterConfig } from 'src/config/multer.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EsportesGame, EsportsTeam])],
+  imports: [
+    TypeOrmModule.forFeature([EsportesGame, EsportsTeam]),
+    MulterModule.register(esportsMulterConfig)
+  ],
   controllers: [EsportsController],
   providers: [EsportsService]
 })
--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -12,5 +12,25 @@ export const multerConfig: MulterModuleOptions = {
       const filename = `${originalName}-${uniqueSuffix}.${fileExtension}`;
       callback(null, filename);
     }
-  })
+  }),
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  }
+};
+
+export const esportsMulterConfig: MulterModuleOptions = {
+  storage: diskStorage({
+    destination: './uploads/esportsteam',
+    filename: (_req, file, callback) => {
+      const originalName = file.originalname.split('.')[0].toLowerCase();
+      const uniqueSuffix = uuidv4();
+      const fileExtension = file.originalname.split('.').pop();
+      const filename = `${file.fieldname}-${originalName}-${uniqueSuffix}.${fileExtension}`;
+
+      callback(null, filename);
+    }
+  }),
+  limits: {
+    fileSize: 1024 * 1024 * 50
+  }
 };
